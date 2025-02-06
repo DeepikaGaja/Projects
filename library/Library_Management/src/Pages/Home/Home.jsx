9539678fdd7da6f62f2d0b60ca318c16fe2d0f20
Deepika_Gajendran <deepigaja@gmail.com>
2025-02-06 20:02:11 +0530

    Add files via upload

---
+++ library/Library_Management/src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Categories from "../../Components/Categories";
+import Booksdata from "../../Components/Booksdata";
+import image2 from '../../assets/image2.png';
+
+
+const Home = () => {
+    return (
+        <div>
+           
+            <Categories />
+            <Booksdata />
+        </div>
+    )}
+export default Home;
